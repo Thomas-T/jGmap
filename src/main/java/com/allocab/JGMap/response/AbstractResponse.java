eedf4559628e76478b1d3e8920779c6373f8f60c
Thomas Tiercelin <thomas.tiercelin@allocab.com>
2015-07-27 19:00:51 +0200

    fix fail on unknown property bug

--- src/main/java/com/allocab/JGMap/response/AbstractResponse.java
+++ src/main/java/com/allocab/JGMap/response/AbstractResponse.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.io.StringWriter;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @SuppressWarnings("serial")
 public abstract class AbstractResponse implements Serializable {
@@ -32,6 +34,10 @@ public abstract class AbstractResponse implements Serializable {
   
   protected static <T extends AbstractResponse> T deserialize(Class<T> clazz, String response) {
     ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.disable(SerializationFeature.WRITE_NULL_MAP_VALUES);
+    objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    objectMapper.disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES);
+    objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);    
     try {
       return objectMapper.readValue(response, clazz);
     } catch (IOException e) {
