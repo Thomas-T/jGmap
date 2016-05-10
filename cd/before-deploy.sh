81a98f1fee5c15f1e5381bc200f2b3fc65ee210b
Thomas Tiercelin <thomas.tiercelin@allocab.com>
2016-05-10 20:25:51 +0200

    fixed wrong place for deploy key

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_31f65867e1eb_key -iv $encrypted_31f65867e1eb_iv -in codesigning.asc.enc -out codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
     gpg --fast-import cd/signingkey.asc
 fi
