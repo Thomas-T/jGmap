1480a18709a808d7c5db44bfe06e715498ddc90e
Thomas Tiercelin <thomas.tiercelin@allocab.com>
2016-05-11 21:28:01 +0200

    reactive test when cobertura launched on travis

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && ([ "$TRAVIS_BRANCH" == 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]); then
-    mvn -DskipTests=true -DcoberturaFormat=xml cobertura:cobertura coveralls:report
+    mvn -DcoberturaFormat=xml cobertura:cobertura coveralls:report
     gpg --import cd/sign-key.gpg
     mvn deploy -P sign,build-extras -DskipTests=true --settings cd/mvnsettings.xml
 fi
