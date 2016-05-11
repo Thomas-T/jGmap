07eff22863f02f5292ab05eb4a40ba1fa45360ab
Thomas Tiercelin <thomas.tiercelin@allocab.com>
2016-05-11 11:45:00 +0200

    replace gpgp key id

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     gpg --import cd/sign-key.gpg
-    mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
+    mvn deploy -P sign,build-extras -DskipTests=true --settings cd/mvnsettings.xml
 fi
