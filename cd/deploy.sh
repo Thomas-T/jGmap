0aa78c312075f073f3524988a49a40134268f9da
Thomas Tiercelin <thomas.tiercelin@allocab.com>
2016-05-11 21:37:05 +0200

    put coverage test for all branches on travis

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+mvn -DcoberturaFormat=xml cobertura:cobertura coveralls:report
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && ([ "$TRAVIS_BRANCH" == 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]); then
-    mvn -DcoberturaFormat=xml cobertura:cobertura coveralls:report
     gpg --import cd/sign-key.gpg
     mvn deploy -P sign,build-extras -DskipTests=true --settings cd/mvnsettings.xml
 fi
