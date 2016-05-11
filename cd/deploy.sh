9a67e6b28fef32b14ab199f87a1ffac5b2d8ba5d
Thomas Tiercelin <thomas.tiercelin@allocab.com>
2016-05-11 22:14:29 +0200

    added coverture report when test on travis

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-mvn -DcoberturaFormat=xml cobertura:cobertura coveralls:report
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && ([ "$TRAVIS_BRANCH" == 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]); then
     gpg --import cd/sign-key.gpg
     mvn deploy -P sign,build-extras -DskipTests=true --settings cd/mvnsettings.xml
