687b132eca6c02afa2088c5ffede53b5fc87f4c6
Thomas Tiercelin <thomas.tiercelin@allocab.com>
2016-05-11 12:06:08 +0200

    redo keysign from start

--- cd/before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_31f65867e1eb_key -iv $encrypted_31f65867e1eb_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-    gpg --fast-import cd/codesigning.asc
-fi
