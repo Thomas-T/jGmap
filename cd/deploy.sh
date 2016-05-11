2d5f142b33ffb64630e7dc6009ca67ed615f232c
Thomas Tiercelin <thomas.tiercelin@allocab.com>
2016-05-11 14:37:30 +0200

    maven deploy only for develop and master

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && ([ "$TRAVIS_BRANCH" == 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]); then
     gpg --import cd/sign-key.gpg
     mvn deploy -P sign,build-extras -DskipTests=true --settings cd/mvnsettings.xml
 fi
